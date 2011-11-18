e60de2d70dd9cf9cb5ebcbe6f6efb8ea739a0ee9
Chris Agocs <camerazn@gmail.com>
2011-11-18 11:34:43 -0500

    Initial Commit

---
+++ src/emergencymanagementapp/EmergencyManagementApp.java
@@ -0,0 +1,44 @@
+/*
+ * EmergencyManagementApp.java
+ */
+
+package emergencymanagementapp;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class EmergencyManagementApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new EmergencyManagementView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of EmergencyManagementApp
+     */
+    public static EmergencyManagementApp getApplication() {
+        return Application.getInstance(EmergencyManagementApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(EmergencyManagementApp.class, args);
+    }
+}
